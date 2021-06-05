5dcee1a540df26208331e5ce642afa1b6280e192
Christian Javellana <cjjavellana@gmail.com>
2021-06-05 17:05:22 +0800

    Fix: compile error

--- internal/ecm/service/objectstore.go
+++ internal/ecm/service/objectstore.go
@@ -21,8 +21,7 @@ func (s *ObjectStoreService) CreateWorkspace(_ context.Context, in *pb.CreateWor
 
 	now := time.Now()
 
-	w := s.ObjectStore.NewWorkspace(in.WorkspaceName)
-	w.SetDescription(in.Description)
+	w := s.ObjectStore.NewWorkspace(in.WorkspaceName, in.Description)
 	w.SetCreatedBy("UserFromAuthToken")
 	w.SetDateCreated(&now)
 
